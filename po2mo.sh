d5ddb03fab5afff6850307a24ce63d3d4527f391
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-09-02 20:10:59 +0300

    colorized

--- po2mo.sh
+++ po2mo.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 source=$1
 target=`echo $source | sed -e 's/\.po$/\.mo/'`
 msgfmt $source -o $target
-echo -e "write $target"
+echo -e "      ${GREEN}write $target${NC}"
